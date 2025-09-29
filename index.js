c9f3eb116c4530387945eba45c6aa8859b9d4d5a
Jahirul Islam <nockjahirul@gmail.com>
2025-09-29 08:09:17 +0600

    make middlewer for varify token

--- index.js
+++ index.js
@@ -138,6 +138,25 @@ async function run() {
   })
     //for jwt token api end 2
 
+    //make middle weres for varify token start
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'});
+      }
+      const token = req.headers.authorization.split(' ')[1] ;
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+       req.decoded = decoded;
+       next();
+      })
+      
+    }
+    //make middle weres for verify token end
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
